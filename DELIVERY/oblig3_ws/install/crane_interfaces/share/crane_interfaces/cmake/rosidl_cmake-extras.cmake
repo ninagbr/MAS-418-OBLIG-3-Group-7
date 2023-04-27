fa86b6b23ee138ebcf477b9c1ebe212119a44957
ninagbr <97841460+ninagbr@users.noreply.github.com>
2023-04-27 12:14:35 +0200

    Delete DELIVERY/oblig3_ws/install directory

--- DELIVERY/oblig3_ws/install/crane_interfaces/share/crane_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(crane_interfaces_IDL_FILES "msg/MotionReference.idl")
-set(crane_interfaces_INTERFACE_FILES "msg/MotionReference.msg")
